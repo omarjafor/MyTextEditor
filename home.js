69996424522b3d4b80bf4638471b69fd406ccf11
omarjafor <omarjaforchy@gmail.com>
2023-08-16 22:43:10 +0600

    text editor

---
+++ home.js
@@ -0,0 +1,47 @@
+function f1() {
+    document.getElementById("textarea1").style.fontWeight = "bold";
+}
+  
+function f2() {
+    document.getElementById("textarea1").style.fontStyle = "italic";
+}
+
+function f3() {
+    document.getElementById("textarea1").style.fontStyle = "underline";
+}
+  
+function f4() {
+    document.getElementById("textarea1").style.textAlign = "left";
+}
+  
+function f5() {
+    document.getElementById("textarea1").style.textAlign = "center";
+}
+  
+function f6() {
+    document.getElementById("textarea1").style.textAlign = "right";
+}
+  
+function f7() {
+    document.getElementById("textarea1").style.textAlign = "justify";
+}
+
+function f8() {
+    document.getElementById("textarea1").style.fontSize = "fontSize";
+}
+
+function f9() {
+    document.getElementById("textarea1").style.textTransform = "uppercase";
+}
+ 
+function f10() {
+    document.getElementById("textarea1").style.color = "capitalize";
+}
+
+function f11() {
+    document.getElementById("textarea1").style.fontWeight = "normal";
+    document.getElementById("textarea1").style.textAlign = "left";
+    document.getElementById("textarea1").style.fontStyle = "normal";
+    document.getElementById("textarea1").style.textTransform = "capitalize";
+    document.getElementById("textarea1").value = " ";
+}
